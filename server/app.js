6fe957db2838139b2d9b60cfc5da2cd78b5c2d65
maria-luisa-garcia <garciatellezmarialuisa@gmail.com>
2022-06-12 11:09:28 -0500

    S30 Agregando mensajes de log

--- server/app.js
+++ server/app.js
@@ -17,10 +17,8 @@ import WebpackHotMiddleware from 'webpack-hot-middleware';
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 import aboutRouter from './routes/about';
-
 // Importando nuestro logger
 import winston from './config/winston';
-
 // Importando modulos de webpack
 // Nucleo de webpack
 // Permite incrustar webpack en express
@@ -65,7 +63,6 @@ if (nodeEnv === 'development') {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 // Todos los middlewares globales
 // van primero que cualquier otro middleware de la app
 app.use(morgan('dev', { stream: winston.stream }));
@@ -78,15 +75,29 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
+  // Registrando el error 404 en el log
+  // winston.error(
+  //   `404 - Not Found: ${req.method} ${req.originalUrl} : IP ${req.ip}`
+  // );
+
   next(createError(404));
 });
+
 // error handler
 app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Registramos el error en winston
+  winston.error(
+    `${err.status || 500} : ${err.message} 
+    : ${req.method} ${req.originalUrl} : IP ${req.ip}`
+  );
+
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- server/bin/www.js
+++ server/bin/www.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
-
 /**
  * Module dependencies.
  */
-
  import app from '@s/app';
  import Debug from 'debug';
- import http from 'http'; 
+ import http from 'http';
+ 
+ // Importando nuestro logger
+ import winston from '../config/winston';
  
  // Creando instancia del debugger
  const debug = Debug('projnotes-2022a:server');
@@ -14,79 +15,63 @@
  /**
   * Get port from environment and store in Express.
   */
- 
-  const port = normalizePort(process.env.PORT || '3000');
-  // app es una instnacia de ExpressJs[ ] [ NODE ]
-  app.set('port', port);
- 
+ const port = normalizePort(process.env.PORT || '3000');
+ // app es una instnacia de ExpressJs[ ] [ NODE ]
+ app.set('port', port);
  /**
   * Create HTTP server.
   */
- 
-  const server = http.createServer(app); // (req, res, next, err)=> {}
- 
+ const server = http.createServer(app); // (req, res, next, err)=> {}
  /**
   * Listen on provided port, on all network interfaces.
   */
- 
  server.listen(port); // Pone al server a escuchar
  // Se registran eventos
-server.on('error', onError); // En caso de error
-server.on('listening', onListening); // Cuando esta escuchando
-
-/**
+ server.on('error', onError); // En caso de error
+ server.on('listening', onListening); // Cuando esta escuchando
+ /**
   * Normalize a port into a number, string, or false.
   */
- 
  function normalizePort(val) {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
+   const port = parseInt(val, 10);
+   if (isNaN(port)) {
      // named pipe
      return val;
    }
- 
    if (port >= 0) {
      // port number
      return port;
    }
- 
    return false;
  }
- 
  /**
   * Event listener for HTTP server "error" event.
   */
- 
  function onError(error) {
-  if (error.syscall !== 'listen') {
+   if (error.syscall !== 'listen') {
      throw error;
    }
- 
-   var bind = typeof port === 'string' ? 'Pipe ${port}` : `Port ${port}`;
-   
+   const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
    // handle specific listen errors with friendly messages
    switch (error.code) {
-    case 'EACCES':
-      console.error(`${bind} requires elevated privileges`);
+     case 'EACCES':
+      winston.error(`Port: ${bind} requires elevated privileges`);
       process.exit(1);
-       break;
-       case 'EADDRINUSE':
-        console.error(`${bind} is already in use`);
-        process.exit(1);
-       break;
-     default:
-       throw error;
-   }
- }
- 
- /**
-  * Event listener for HTTP server "listening" event.
-  */
- 
- function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
-  debug(`Listening on ${bind}`);
-  console.log(`✍ Servidor escuchando 🤖🦻...en ${app.get('port')}`); 
-}
+      break;
+    case 'EADDRINUSE':
+      winston.error(`Port: ${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+    }
+  }
+  /**
+   * Event listener for HTTP server "listening" event.
+   */
+  function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+    debug(`Listening on ${bind}`);
+    winston.info('Servidor escuchando 🤖🦻...en ${app.get('port')}');
+  }
