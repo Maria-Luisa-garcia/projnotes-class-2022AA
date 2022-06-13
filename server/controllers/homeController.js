e8f1b8d4385d9b97b5f420ed2d2604d58ceeb98f
maria-luisa-garcia <garciatellezmarialuisa@gmail.com>
2022-06-12 20:58:45 -0500

    S38 About increment

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -30,6 +30,9 @@ const about = (req, res) => {
     name: 'Ing Garcia Tellez Maria Luisa',
     email: 'L181130089@gamadero.tecnm.mx',
     url: 'https://github.com/Maria-Luisa-garcia/projnotes-class-2022AA',
+    description:
+    'Aplicación que te permite registrar ideas de proyectos. PwpcII-2022A',
+  version: '0.0.alpha',
   });
 };
 
