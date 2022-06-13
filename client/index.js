6e68949900cda5244b234d3fd92c5417647f87c5
maria-luisa-garcia <garciatellezmarialuisa@gmail.com>
2022-06-13 02:02:49 -0500

    S41 Agregando Menu Dropdown

--- client/index.js
+++ client/index.js
@@ -1,14 +1,18 @@
 /* eslint-disable no-console */
 /* global M */
-
 // Incorporando estilos
 // a mi bundle
 import './styles/mystyle.css';
 
 /* Inicializando elementos de materializecss */
 document.addEventListener('DOMContentLoaded', () => {
+  /* Inicializando los sideNavas */
   // Obteniendo la referencia a la barra de navegacion
   // lateral
   const sideNavs = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sideNavs);
+
+  /* Inicializando los dropdown */
+  const dropdowns = document.querySelectorAll('.dropdown-trigger');
+  M.Dropdown.init(dropdowns);
 });
