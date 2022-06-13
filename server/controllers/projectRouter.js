14feb45de14ee223c550ad080ae5030150740302
maria-luisa-garcia <garciatellezmarialuisa@gmail.com>
2022-06-13 03:02:17 -0500

    S43 Formulario registrar proyecto

--- server/controllers/projectRouter.js
+++ server/controllers/projectRouter.js
@@ -3,19 +3,19 @@
 // Lista los proyectos
 // GET /projects | GET /projects/index
 const index = (req, res) => {
-    res.send('Listando proyectos 🚧');
-    // TODO: Agregar codigo de listado de proyectos
-  };
-  
-  // Agrega ideas de proyectos
-  // GET /projects/add
-  const add = (req, res) => {
-    res.send('Agreando ideas de proyectos 🚧');
-    // TODO: Agregar codigo para agregar proyectos
-  };
-  
-  // Exportando el controlador
-  export default {
-    index,
-    add,
-  };
+  res.send('Listando proyectos 🚧');
+  // TODO: Agregar codigo de listado de proyectos
+};
+
+// Agrega ideas de proyectos
+// GET /projects/add
+const add = (req, res) => {
+  res.send('Agreando ideas de proyectos 🚧');
+  // TODO: Agregar codigo para agregar proyectos
+};
+
+// Exportando el controlador
+export default {
+  index,
+  add,
+};
