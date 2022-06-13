ef286f2dc1cf9638672d3820f9c634a7e63baff6
maria-luisa-garcia <garciatellezmarialuisa@gmail.com>
2022-06-12 19:19:28 -0500

    S36 About Route

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,30 +1,39 @@
 // URL: Get /
 const index = (req, res) => {
-    // Calculando emojie
-    const emojieDataset = [
-      '💻',
-      '👨‍💻',
-      '🎈',
-      '🎄',
-      '🦺',
-      '🎢',
-      '🎁',
-      '🚆',
-      '🌍',
-      '♥',
-    ];
-    const emojie =
-      emojieDataset[Math.floor(Math.random() * emojieDataset.length)];
-    // View-Models
-    const viewModel = {
-      title: 'Index Controller Working!!!',
-      author: 'Ivan Rivalcoba',
-      emojie,
-    };
-    res.render('index', viewModel);
+  // Calculando emojie
+  const emojieDataset = [
+    '💻',
+    '👨‍💻',
+    '🎈',
+    '🎄',
+    '🦺',
+    '🎢',
+    '🎁',
+    '🚆',
+    '🌍',
+    '♥',
+  ];
+  const emojie =
+    emojieDataset[Math.floor(Math.random() * emojieDataset.length)];
+  // View-Models
+  const viewModel = {
+    title: 'Index Controller Working!!!',
+    author: 'Garcia Tellez Maria Luisa',
+    emojie,
   };
-  
-  export default {
-    // Action Methods
-    index,
-  };
+  res.render('index', viewModel);
+};
+// URL: Get /about
+const about = (req, res) => {
+  res.render('home/aboutView', {
+    name: 'Ing Garcia Tellez Maria Luisa',
+    email: 'L181130089@gamadero.tecnm.mx',
+    url: 'https://github.com/Maria-Luisa-garcia/projnotes-class-2022AA',
+  });
+};
+
+export default {
+  // Action Methods
+  index,
+  about,
+};
