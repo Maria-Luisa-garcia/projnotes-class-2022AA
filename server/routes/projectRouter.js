3b6aa9ead5b6e887df6d1e6db85f7b83e9f4757f
maria-luisa-garcia <garciatellezmarialuisa@gmail.com>
2022-06-14 21:13:34 -0500

    Error en la base de datos

--- server/routes/projectRouter.js
+++ server/routes/projectRouter.js
@@ -1,6 +1,11 @@
 // Importando el Router de Express
 import { Router } from 'express';
 
+// Importar el validador
+import Validate from '../validators/validateFactory';
+// Importamos el esquema de validación
+import projectValidator from '../validators/projectValidator';
+
 // Importar el controlador de proyectos
 import projectController from '../controllers/projectController';
 
@@ -15,6 +20,18 @@ router.get(['/', '/index'], projectController.index);
 // Envia el formulario para registrar una idea de proyecto
 // get /projects/add
 router.get('/add', projectController.add);
+
+/* ------ POST ------ */
+// Procesa el formulario que Agrega ideas de proyectos
+// POST /projects/add
+router.post(
+  '/add',
+  Validate({
+    shape: projectValidator.projectSchema,
+    getObject: projectValidator.getProject,
+  }),
+  projectController.addPost
+);
 
 // Exportando el enrutador Projects
 export default router;
